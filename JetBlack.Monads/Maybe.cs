6b4fe246fb1589f607dc51949270e0d0310e5466
rob-blackbourn <rob.blackbourn@googlemail.com>
2015-05-11 08:11:14 +0100

    Moved the extensions into the main class.

--- JetBlack.Monads/Maybe.cs
+++ JetBlack.Monads/Maybe.cs
@@ -9,6 +9,16 @@ namespace JetBlack.Monads
         {
             return value;
         }
+    
+        public static Maybe<TResult> Bind<T, TResult>(this Maybe<T> value, Func<T, Maybe<TResult>> apply)
+        {
+            return value.HasValue ? apply(value.Value) : Maybe<TResult>.Nothing;
+        }
+
+        public static Maybe<TResult> SelectMany<T, TIntermediate, TResult>(this Maybe<T> value, Func<T, Maybe<TIntermediate>> transform, Func<T, TIntermediate, TResult> project)
+        {
+            return value.Bind(x => transform(x).Bind(y => Return(project(x, y))));
+        }
     }
 
     public struct Maybe<T> : IEquatable<Maybe<T>>, IEquatable<T>, IDisposable
