c632b1ef1093b57fccb8ae1d0a0089239043eaa2
rob-blackbourn <rob.blackbourn@googlemail.com>
2015-07-30 08:52:29 +0100

    Changed the solution name

---
+++ JetBlack.Monads.Test/EitherTest.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using NUnit.Framework;
+
+namespace JetBlack.Monads.Test
+{
+    [TestFixture]
+    public class EitherTest
+    {
+        [Test]
+        public void ConstructorTest()
+        {
+            var e0 = new Either<int,double> ();
+            Assert.IsFalse(e0.IsValid);
+            var e1 = Either.Left<int, double>(1);
+            Assert.IsTrue(e1.IsLeft);
+            var e2 = Either.Right<int, double>(1);
+            Assert.IsTrue(e2.IsRight);
+        }
+
+        [Test]
+        public void TestFactorial()
+        {
+            var calculator = new FactorialCalculator();
+
+            var expected = Enumerable.Range(1, 10).Select(i => new {i, r = calculator.Factorial(i)}).ToDictionary(key => key.i, value => value.r);
+
+            foreach (var pair in expected)
+                Assert.AreEqual(pair.Value, calculator.Calculate(pair.Key).Match(x => x, x => x.Value));
+
+            foreach (var pair in expected)
+                Assert.AreEqual(pair.Value, calculator.Calculate(pair.Key).Match(x => x, x => x.Value));
+        }
+
+        public class FactorialCalculator
+        {
+            private readonly IDictionary<int,int> _cache = new Dictionary<int, int>();
+
+            public Either<int, Lazy<int>> Calculate(int i)
+            {
+                int factorial;
+                if (_cache.TryGetValue(i, out factorial))
+                    return Either.Left<int, Lazy<int>>(factorial);
+                
+                return Either.Right<int, Lazy<int>>(new Lazy<int>(() =>
+                {
+                    factorial = Factorial(i);
+                    _cache[i] = factorial;
+                    return factorial;
+                }));
+            }
+
+            public int Factorial(int i)
+            {
+                if (i <= 1)
+                    return 1;
+                return i * Factorial(i - 1);
+            }
+        }
+    }
+}
---
+++ JetBlack.Monads/Either.cs
@@ -0,0 +1,93 @@
+﻿using System;
+
+namespace JetBlack.Monads
+{
+    public static class Either
+    {
+        public static Either<TLeft, TRight> Left<TLeft, TRight>(TLeft left)
+        {
+            return new Either<TLeft, TRight>(left, default(TRight), true);
+        }
+
+        public static Either<TLeft, TRight> Right<TLeft, TRight>(TRight right)
+        {
+            return new Either<TLeft, TRight>(default(TLeft), right, false);
+        }
+    }
+
+    public struct Either<TLeft, TRight>
+    {
+        private readonly TLeft _left;
+        private readonly TRight _right;
+        private readonly bool? _isLeft;
+
+        internal Either(TLeft left, TRight right, bool isLeft)
+            : this()
+        {
+            _left = left;
+            _right = right;
+            _isLeft = isLeft;
+        }
+
+        public bool IsValid { get { return _isLeft.HasValue; } }
+
+        public bool IsLeft
+        {
+            get
+            {
+                if (!_isLeft.HasValue) throw new InvalidOperationException();
+                return _isLeft.Value;
+            }
+        }
+
+        public bool IsRight { get { return !IsLeft; } }
+
+        public TLeft Left
+        {
+            get
+            {
+                if (IsLeft) return _left;
+                throw new InvalidOperationException();
+            }
+        }
+
+        public TRight Right
+        {
+            get
+            {
+                if (IsRight) return _right;
+                throw new InvalidOperationException();
+            }
+        }
+
+        public T Match<T>(Func<TLeft, T> left, Func<TRight, T> right)
+        {
+            return IsLeft ? MatchLeft(left) : MatchRight(right);
+        }
+
+        public T MatchLeft<T>(Func<TLeft, T> left)
+        {
+            return left(Left);
+        }
+
+        public T MatchRight<T>(Func<TRight, T> right)
+        {
+            return right(Right);
+        }
+
+        public T MatchLeft<T>(Func<TLeft, T> left, T defaultValue)
+        {
+            return IsLeft ? left(Left) : defaultValue;
+        }
+
+        public T MatchRight<T>(Func<TRight, T> right, T defaultValue)
+        {
+            return IsLeft ? defaultValue : right(Right);
+        }
+
+        public override string ToString()
+        {
+            return !IsValid ? "Invalid" : IsLeft ? "Left: " + Left : "Right: " + Right;
+        }
+    }
+}
